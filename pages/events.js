a66dbf59d81b2069e269b9c276a3ffacc25ab87e
harishbabusdc <harish.babu@sdc.company>
2021-07-25 18:55:17 +0530

    app url

--- pages/events.js
+++ pages/events.js
@@ -36,7 +36,7 @@ function events({data}) {
 // }
 
 events.getInitialProps = async (ctx) => {
-  const res = await fetch('https://us-api.bessppl.com/events')
+  const res = await fetch('https://app.bessppl.com/events')
   const data = await res.json()
   return { data }
 }
